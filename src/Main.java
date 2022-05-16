4cfb7d794ff44ad4a6aac5687f700b403ee72a03
Diyaz007 <diyaz18.08@gmail.com>
2022-05-16 22:03:46 +0600

    A

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Son1 Diyaz = new Son1("Дияз", 15);
+        Son2 Ilyaz = new Son2("Ильяз",12);
+        Son3 Shamil = new Son3("Шамиль", 11);
+
+        System.out.println(Diyaz);
+        System.out.println(Ilyaz);
+        System.out.println(Shamil);
+    }
+}
---
+++ src/Son1.java
@@ -0,0 +1,15 @@
+public class Son1 extends Dad {
+    public Son1() {
+            super();
+    }
+    public Son1(String name, int age) {
+            super(name, age);
+    }
+    @Override
+    public String toString() {
+        return "Son1 [Name = " + getName() + ", Age = " + getAge()+"]";
+        }
+
+
+
+    }
